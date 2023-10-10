8ab1a5704ac56c951170d6f2c44be3c823839336
teja <autotesting@gmail.com>
2023-10-10 08:17:23 +0530

    logout module added

---
+++ src/test/java/modules/LogoutModule.java
@@ -0,0 +1,14 @@
+package modules;
+
+import org.testng.annotations.Test;
+
+public class LogoutModule {
+	
+	
+	@Test
+	public void logoutTestcase()
+	{
+		System.out.println("log out testcase");
+	}
+
+}
